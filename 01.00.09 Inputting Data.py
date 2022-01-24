888c86bb9fb9d109d91eceec7262d5a0d4c82a89
parikshya098 <pbhandari1@ualr.edu>
2022-01-24 16:33:09 +0000

    Unit 1

---
+++ 01.00.09 Inputting Data.py
@@ -0,0 +1,5 @@
+firstname = input("Enter your first name: ")  # Input statement without a prompt
+print("You Entered: " + firstname)
+
+lastname = input("Enter your last name: ")  # Input statement with a prompt
+print("You Entered: " + lastname)
---
+++ 01.01 Sum of Three Numbers.py
@@ -0,0 +1,8 @@
+x = input("First number: ")
+y = input("Second number: ")
+z = input("Third number: ")
+firstnumber = int(x)
+secondnumber = int(y)
+thirdnumber = int(z)
+total = firstnumber + secondnumber + thirdnumber
+print(total)
