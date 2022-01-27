2c7f007adab331fb4cb41b37ef9aaefaa637e522
parikshya098 <pbhandari1@ualr.edu>
2022-01-27 05:06:57 +0000

    Unit 1 A

--- 01.10 Average Speed.py
+++ 01.10 Average Speed.py
@@ -1,4 +1,9 @@
-lngth = int(input("Enter length of race in kilometers: "))
-time = int(input("Enter minutes: "))
-sec_time = int(input("Enter seconds: "))
+x = int(input("Enter length of race in kilometers: "))
+y = int(input("Enter minutes: "))
+z = int(input("Enter seconds: "))
 
+distance = x * 0.621 # 1km = 0.621371 miles
+time = y / 60 + z / 3600 # converting minutes and seconds into hours
+
+average_speed = distance / time # speed is distance divided by time
+print(average_speed)
