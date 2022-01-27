6e6bd14a345df29d44a839c7502e15dcd9609eac
parikshya098 <pbhandari1@ualr.edu>
2022-01-27 03:32:43 +0000

    Unit 1 Previous and Next

--- 01.05 Previous and Next.py
+++ 01.05 Previous and Next.py
@@ -1,1 +1,8 @@
-x = int(input("Enter a number: "))
+digit = int(input("Enter a number: "))
+x = digit + 1
+y = digit - 1
+first = "The next number for the number " + str(digit) + " is " + str(x) + "."
+second = "The previous number for the number " + str(digit) + " is " + str(y) + "."
+print(first)
+print(second)
+
